41ffc47556728a6f0dc67c69f678907ed2491282
Pavel_F <paulkrut@gmail.com>
2025-07-04 22:25:10 +0300

    ux

--- src/app/hr/candidates/compare/page.tsx
+++ src/app/hr/candidates/compare/page.tsx
@@ -2,7 +2,7 @@ import dynamic from 'next/dynamic';
 import { Suspense } from 'react';
 import CompareClient from './CompareClient';
 
-export default function Page({ searchParams }: { searchParams: { [key:string]: string|undefined }}){
+export default function Page({ searchParams }: { params?: any; searchParams?: { [key:string]: string | string[] | undefined }}){
   const ids = searchParams.ids ?? null;
   return (
     <Suspense>
